bd039c14f74ff925e656411279df5f474ee7dce3
m00dy <professor.moody@pm.me>
2025-12-09 12:56:30 -0500

    Update adaptfs.ps1

--- adaptfs.ps1
+++ adaptfs.ps1
@@ -1,5 +1,246 @@
-function Get-ADAPTDomainFileServer {
+function Get-ADAPTDomain {
+    [OutputType([System.DirectoryServices.ActiveDirectory.Domain])]
+    [CmdletBinding()]
+    Param(
+        [Parameter(Position = 0, ValueFromPipeline = $True)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Domain,
 
+        [Management.Automation.PSCredential]
+        [Management.Automation.CredentialAttribute()]
+        $Credential = [Management.Automation.PSCredential]::Empty
+    )
+
+    PROCESS {
+        if ($PSBoundParameters['Credential']) {
+            Write-Verbose '[Get-ADAPTDomain] Using alternate credentials for Get-ADAPTDomain'
+
+            if ($PSBoundParameters['Domain']) {
+                $TargetDomain = $Domain
+            }
+            else {
+                # if no domain is supplied, extract the logon domain from the PSCredential passed
+                $TargetDomain = $Credential.GetNetworkCredential().Domain
+                Write-Verbose "[Get-ADAPTDomain] Extracted domain '$TargetDomain' from -Credential"
+            }
+
+            $DomainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Domain', $TargetDomain, $Credential.UserName, $Credential.GetNetworkCredential().Password)
+
+            try {
+                [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($DomainContext)
+            }
+            catch {
+                Write-Verbose "[Get-ADAPTDomain] The specified domain '$TargetDomain' does not exist, could not be contacted, there isn't an existing trust, or the specified credentials are invalid: $_"
+            }
+        }
+        elseif ($PSBoundParameters['Domain']) {
+            $DomainContext = New-Object System.DirectoryServices.ActiveDirectory.DirectoryContext('Domain', $Domain)
+            try {
+                [System.DirectoryServices.ActiveDirectory.Domain]::GetDomain($DomainContext)
+            }
+            catch {
+                Write-Verbose "[Get-ADAPTDomain] The specified domain '$Domain' does not exist, could not be contacted, or there isn't an existing trust : $_"
+            }
+        }
+        else {
+            try {
+                [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain()
+            }
+            catch {
+                Write-Verbose "[Get-ADAPTDomain] Error retrieving the current domain: $_"
+            }
+        }
+    }
+}
+
+function Get-ADAPTDomainSearcher {
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '')]
+    [OutputType('System.DirectoryServices.DirectorySearcher')]
+    [CmdletBinding()]
+    Param(
+        [Parameter(ValueFromPipeline = $True)]
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $Domain,
+
+        [ValidateNotNullOrEmpty()]
+        [Alias('Filter')]
+        [String]
+        $LDAPFilter,
+
+        [ValidateNotNullOrEmpty()]
+        [String[]]
+        $Properties,
+
+        [ValidateNotNullOrEmpty()]
+        [Alias('ADSPath')]
+        [String]
+        $SearchBase,
+
+        [ValidateNotNullOrEmpty()]
+        [String]
+        $SearchBasePrefix,
+
+        [ValidateNotNullOrEmpty()]
+        [Alias('DomainController')]
+        [String]
+        $Server,
+
+        [ValidateSet('Base', 'OneLevel', 'Subtree')]
+        [String]
+        $SearchScope = 'Subtree',
+
+        [ValidateRange(1, 10000)]
+        [Int]
+        $ResultPageSize = 200,
+
+        [ValidateRange(1, 10000)]
+        [Int]
+        $ServerTimeLimit = 120,
+
+        [ValidateSet('Dacl', 'Group', 'None', 'Owner', 'Sacl')]
+        [String]
+        $SecurityMasks,
+
+        [Switch]
+        $Tombstone,
+
+        [Management.Automation.PSCredential]
+        [Management.Automation.CredentialAttribute()]
+        $Credential = [Management.Automation.PSCredential]::Empty
+    )
+
+    PROCESS {
+        if ($PSBoundParameters['Domain']) {
+            $TargetDomain = $Domain
+
+            if ($ENV:USERDNSDOMAIN -and ($ENV:USERDNSDOMAIN.Trim() -ne '')) {
+                # see if we can grab the user DNS logon domain from environment variables
+                $UserDomain = $ENV:USERDNSDOMAIN
+                if ($ENV:LOGONSERVER -and ($ENV:LOGONSERVER.Trim() -ne '') -and $UserDomain) {
+                    $BindServer = "$($ENV:LOGONSERVER -replace '\\','').$UserDomain"
+                }
+            }
+        }
+        elseif ($PSBoundParameters['Credential']) {
+            # if not -Domain is specified, but -Credential is, try to retrieve the current domain name with Get-ADAPTDomain
+            $DomainObject = Get-ADAPTDomain -Credential $Credential
+            $BindServer = ($DomainObject.PdcRoleOwner).Name
+            $TargetDomain = $DomainObject.Name
+        }
+        elseif ($ENV:USERDNSDOMAIN -and ($ENV:USERDNSDOMAIN.Trim() -ne '')) {
+            # see if we can grab the user DNS logon domain from environment variables
+            $TargetDomain = $ENV:USERDNSDOMAIN
+            if ($ENV:LOGONSERVER -and ($ENV:LOGONSERVER.Trim() -ne '') -and $TargetDomain) {
+                $BindServer = "$($ENV:LOGONSERVER -replace '\\','').$TargetDomain"
+            }
+        }
+        else {
+            # otherwise, resort to Get-ADAPTDomain to retrieve the current domain object
+            $DomainObject = Get-ADAPTDomain
+            $BindServer = ($DomainObject.PdcRoleOwner).Name
+            $TargetDomain = $DomainObject.Name
+        }
+
+        if ($PSBoundParameters['Server']) {
+            # if there's not a specified server to bind to, try to pull a logon server from ENV variables
+            $BindServer = $Server
+        }
+
+        $SearchString = 'LDAP://'
+
+        if ($BindServer -and ($BindServer.Trim() -ne '')) {
+            $SearchString += $BindServer
+            if ($TargetDomain) {
+                $SearchString += '/'
+            }
+        }
+
+        if ($PSBoundParameters['SearchBasePrefix']) {
+            $SearchString += $SearchBasePrefix + ','
+        }
+
+        if ($PSBoundParameters['SearchBase']) {
+            if ($SearchBase -Match '^GC://') {
+                # if we're searching the global catalog, get the path in the right format
+                $DN = $SearchBase.ToUpper().Trim('/')
+                $SearchString = ''
+            }
+            else {
+                if ($SearchBase -match '^LDAP://') {
+                    if ($SearchBase -match "LDAP://.+/.+") {
+                        $SearchString = ''
+                        $DN = $SearchBase
+                    }
+                    else {
+                        $DN = $SearchBase.SubString(7)
+                    }
+                }
+                else {
+                    $DN = $SearchBase
+                }
+            }
+        }
+        else {
+            # transform the target domain name into a distinguishedName if an ADS search base is not specified
+            if ($TargetDomain -and ($TargetDomain.Trim() -ne '')) {
+                $DN = "DC=$($TargetDomain.Replace('.', ',DC='))"
+            }
+        }
+
+        $SearchString += $DN
+        Write-Verbose "[Get-ADAPTDomainSearcher] search base: $SearchString"
+
+        if ($Credential -ne [Management.Automation.PSCredential]::Empty) {
+            Write-Verbose "[Get-ADAPTDomainSearcher] Using alternate credentials for LDAP connection"
+            # bind to the inital search object using alternate credentials
+            $DomainObject = New-Object DirectoryServices.DirectoryEntry($SearchString, $Credential.UserName, $Credential.GetNetworkCredential().Password)
+            $Searcher = New-Object System.DirectoryServices.DirectorySearcher($DomainObject)
+        }
+        else {
+            # bind to the inital object using the current credentials
+            $Searcher = New-Object System.DirectoryServices.DirectorySearcher([ADSI]$SearchString)
+        }
+
+        $Searcher.PageSize = $ResultPageSize
+        $Searcher.SearchScope = $SearchScope
+        $Searcher.CacheResults = $False
+        $Searcher.ReferralChasing = [System.DirectoryServices.ReferralChasingOption]::All
+
+        if ($PSBoundParameters['ServerTimeLimit']) {
+            $Searcher.ServerTimeLimit = $ServerTimeLimit
+        }
+
+        if ($PSBoundParameters['Tombstone']) {
+            $Searcher.Tombstone = $True
+        }
+
+        if ($PSBoundParameters['LDAPFilter']) {
+            $Searcher.filter = $LDAPFilter
+        }
+
+        if ($PSBoundParameters['SecurityMasks']) {
+            $Searcher.SecurityMasks = Switch ($SecurityMasks) {
+                'Dacl' { [System.DirectoryServices.SecurityMasks]::Dacl }
+                'Group' { [System.DirectoryServices.SecurityMasks]::Group }
+                'None' { [System.DirectoryServices.SecurityMasks]::None }
+                'Owner' { [System.DirectoryServices.SecurityMasks]::Owner }
+                'Sacl' { [System.DirectoryServices.SecurityMasks]::Sacl }
+            }
+        }
+
+        if ($PSBoundParameters['Properties']) {
+            # handle an array of properties to load w/ the possibility of comma-separated strings
+            $PropertiesToLoad = $Properties| ForEach-Object { $_.Split(',') }
+            $Null = $Searcher.PropertiesToLoad.AddRange(($PropertiesToLoad))
+        }
+
+        $Searcher
+    }
+}
+
+function Get-ADAPTDomainFileServer {
     [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSShouldProcess', '')]
     [OutputType([String])]
     [CmdletBinding()]
@@ -46,8 +287,8 @@ function Get-ADAPTDomainFileServer {
     )
 
     BEGIN {
-        # Renamed internal helper to avoid conflict with native Split-Path
         function Split-ADAPTUNCPath {
+            # short internal helper to split UNC server paths
             Param([String]$Path)
 
             if ($Path -and ($Path.split('\\').Count -ge 3)) {
@@ -75,25 +316,14 @@ function Get-ADAPTDomainFileServer {
         if ($PSBoundParameters['Domain']) {
             ForEach ($TargetDomain in $Domain) {
                 $SearcherArguments['Domain'] = $TargetDomain
-                # Calls the helper function from Script #1
                 $UserSearcher = Get-ADAPTDomainSearcher @SearcherArguments
-                
-                $(ForEach($UserResult in $UserSearcher.FindAll()) {
-                    if ($UserResult.Properties['homedirectory']) {Split-ADAPTUNCPath($UserResult.Properties['homedirectory'])}
-                    if ($UserResult.Properties['scriptpath']) {Split-ADAPTUNCPath($UserResult.Properties['scriptpath'])}
-                    if ($UserResult.Properties['profilepath']) {Split-ADAPTUNCPath($UserResult.Properties['profilepath'])}
-                }) | Sort-Object -Unique
+                # get all results w/o the pipeline and uniquify them (I know it's not pretty)
+                $(ForEach($UserResult in $UserSearcher.FindAll()) {if ($UserResult.Properties['homedirectory']) {Split-ADAPTUNCPath($UserResult.Properties['homedirectory'])}if ($UserResult.Properties['scriptpath']) {Split-ADAPTUNCPath($UserResult.Properties['scriptpath'])}if ($UserResult.Properties['profilepath']) {Split-ADAPTUNCPath($UserResult.Properties['profilepath'])}}) | Sort-Object -Unique
             }
         }
         else {
-            # Calls the helper function from Script #1
             $UserSearcher = Get-ADAPTDomainSearcher @SearcherArguments
-            
-            $(ForEach($UserResult in $UserSearcher.FindAll()) {
-                if ($UserResult.Properties['homedirectory']) {Split-ADAPTUNCPath($UserResult.Properties['homedirectory'])}
-                if ($UserResult.Properties['scriptpath']) {Split-ADAPTUNCPath($UserResult.Properties['scriptpath'])}
-                if ($UserResult.Properties['profilepath']) {Split-ADAPTUNCPath($UserResult.Properties['profilepath'])}
-            }) | Sort-Object -Unique
+            $(ForEach($UserResult in $UserSearcher.FindAll()) {if ($UserResult.Properties['homedirectory']) {Split-ADAPTUNCPath($UserResult.Properties['homedirectory'])}if ($UserResult.Properties['scriptpath']) {Split-ADAPTUNCPath($UserResult.Properties['scriptpath'])}if ($UserResult.Properties['profilepath']) {Split-ADAPTUNCPath($UserResult.Properties['profilepath'])}}) | Sort-Object -Unique
         }
     }
 }
